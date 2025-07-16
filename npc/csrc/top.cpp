1ca4d76031ed425af981a3a25302f0c81730d339
tracer-ysyx <tracer@ysyx.org>
2025-07-16 14:20:58 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:20:58 up 24 min,  1 user,  load average: 0.08, 0.08, 0.15

--- npc/csrc/top.cpp
+++ npc/csrc/top.cpp
@@ -6,33 +6,51 @@
 #include <assert.h>
 
 #include "verilated_vcd_c.h"
+#include <nvboard.h>
+
+
+//static TOP_NAME dut;
+void nvboard_bind_all_pins(TOP_NAME* dut);
+
+static void single_cycle(TOP_NAME* dut) {
+  dut->clk = 0; dut->eval();
+  dut->clk = 1; dut->eval();
+}
+
+static void reset(int n,TOP_NAME* dut) {
+  dut->rst = 1;
+  while (n -- > 0) single_cycle(dut);
+  dut->rst = 0;
+}
 
 int main(int argc, char** argv) {
     VerilatedContext* contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
     Vtop* top = new Vtop{contextp};
 
+		nvboard_bind_all_pins(top);
+    nvboard_init();
+
 		VerilatedVcdC* tfp = new VerilatedVcdC;
 		contextp->traceEverOn(true);//打开追踪功能
     top->trace(tfp, 99);  // 追踪所有信号（深度 99）
     tfp->open("wave.vcd"); // 生成 wave.vcd 文件
-
+		
+		reset(10,top);
 
     while (!contextp->gotFinish()) { 
-				int a = rand() & 1;
- 				int b = rand() & 1;
- 	 			top->a = a;
- 	 			top->b = b;
+				
  	 			top->eval();
-  			printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-
 				tfp->dump(contextp->time());//dump wave
 				contextp->timeInc(1);//推动仿真时间
-
-	  		assert(top->f == (a ^ b));
+				nvboard_update();
+				single_cycle(top);
  
 }
+		tfp->close();
+		nvboard_quit();
     delete top;
     delete contextp;
+		delete tfp;
     return 0;
 }
