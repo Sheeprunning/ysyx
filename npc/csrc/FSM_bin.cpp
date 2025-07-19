5763235ba7eb844c981fb3e0d1caf9c16fc6c4f8
tracer-ysyx <tracer@ysyx.org>
2025-07-19 23:49:08 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:49:07 up  8:12,  1 user,  load average: 0.49, 0.31, 0.21

---
+++ npc/csrc/FSM_bin.cpp
@@ -0,0 +1,55 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "VFSM_bin.h"
+
+#include <iostream>
+using namespace std;
+
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
+
+static TOP_NAME* top;
+
+void step_and_dump_wave(){
+  top->eval();
+  contextp->timeInc(10);
+  tfp->dump(contextp->time());
+}
+void sim_init(){
+  contextp = new VerilatedContext;
+  tfp = new VerilatedVcdC;
+  top = new TOP_NAME{contextp};
+  contextp->traceEverOn(true);
+  top->trace(tfp, 99);
+  tfp->open("wave.vcd");
+}
+static void single_cycle() {
+  top->clk = 0; top->eval();
+  top->clk = 1; top->eval();
+}
+
+static void reset(int n) {
+  top->rst = 1;
+  while (n -- > 0) single_cycle();
+  top->rst = 0;
+}
+void sim_exit(){
+  step_and_dump_wave();
+  tfp->close();
+  delete top;
+  delete tfp;
+}
+
+int main() {
+  sim_init();
+  reset(10);  top->in=1;  single_cycle();
+    top->in=1;  single_cycle();
+    top->in=1;  single_cycle();
+    top->in=1;  single_cycle();
+    top->in=0;  single_cycle();
+    top->in=0;  single_cycle();
+    top->in=0;  single_cycle();
+    top->in=0;  single_cycle();
+    top->in=0;  single_cycle();
+  sim_exit();
+}
