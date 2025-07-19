8cf46b437538d996ddd6fef2a55f946d11fd38f3
tracer-ysyx <tracer@ysyx.org>
2025-07-19 20:55:19 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:55:19 up  5:33,  1 user,  load average: 0.65, 0.71, 0.45

--- npc/vsrc/shift_register8.v
+++ npc/vsrc/shift_register8.v
@@ -7,11 +7,18 @@ module shift_register8(
     output f//标志是否全零
 );
 reg t;
-reg [7:0]cin=0;
+
 always @(posedge clk)begin
-    cin<=set?din:dout;
-    t<=cin[4]^cin[3]^cin[2]^cin[0];
-    dout<=direction?{t,cin[7:1]}:{cin[6:0],t};
-    f<=(dout==0)?0:1;
+    if(set)begin
+      dout<=din;
+      t<=dout[4]^dout[3]^dout[2]^dout[0];
+    end
+    else begin
+        t<=dout[4]^dout[3]^dout[2]^dout[0];
+        dout<=direction?{t,dout[7:1]}:{dout[6:0],t};
+        
+    end
 end
+
+assign f=(dout==0)?0:1;
 endmodule
