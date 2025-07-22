1454be542e8ed8828b12a4b94375d1de4e1fc74d
tracer-ysyx <tracer@ysyx.org>
2025-07-22 23:15:35 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:15:35 up  8:45,  1 user,  load average: 0.58, 0.42, 0.44

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -139,7 +139,7 @@ word_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  //TODO();
   for(int i=0;i<nr_token;i++){
     printf("%s ",tokens[i].str);
   }
