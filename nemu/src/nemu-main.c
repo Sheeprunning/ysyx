199524cd17ba2f2d286676e304bc21669114f2b8
tracer-ysyx <tracer@ysyx.org>
2025-07-23 22:31:47 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:31:47 up  7:15,  1 user,  load average: 0.20, 0.35, 0.28

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -37,8 +37,8 @@ int test(){
     bool *success = &flag;
   
     while (fscanf(file, "%u %s\n", &num, str) == 2) {
-      printf("%u '%s'计算结果为",num, str);  // 输出第二个字段（字符串）
-      //expr(str,success);
+      printf("%s = %u 计算结果为", str,num);  // 输出第二个字段（字符串）
+      expr(str,success);
     }
  
     fclose(file);
