ce3bdb7eaa49b0e052835bfd042f154b23bedf9b
tracer-ysyx <tracer@ysyx.org>
2025-07-24 17:56:21 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:56:21 up  1:05,  0 users,  load average: 0.44, 0.37, 0.38

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,7 +24,7 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-  printf("reg_name reg_i reg_value-----------");
+  printf("reg_name reg_i reg_value-----------\n");
   for(int i=0;i<32;i++){
     printf("%8s %4d %8d\n",regs[i],i,cpu.gpr[i]);
   }
