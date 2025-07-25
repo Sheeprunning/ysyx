345d3fa02dd2125fc4ebf2b7aac901ba57b55131
tracer-ysyx <tracer@ysyx.org>
2025-07-25 20:08:51 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:08:51 up  5:55,  1 user,  load average: 0.29, 0.15, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -343,8 +343,8 @@ char* eval(int p, int q) {
     int val1,val2;
     int op = tokens[op_pos].type;
     val1_str=eval(p, op_pos - 1);
-    val2_str=eval(op_pos + 1, q);
-    if(!val2_str){//val1没有可能是非的计算
+    val2_str=eval(op_pos + 1, q);printf("%s\n",val2_str);
+    if(!val2_str){//val1有可能是非的计算
       return NULL;
     }
     if(val1_str[0]=='0'){
@@ -352,7 +352,7 @@ char* eval(int p, int q) {
     }else sscanf(val1_str,"%d",&val1);
     if(val2_str[0]=='0'){
       sscanf(val2_str,"%x",&val2);
-    }else sscanf(val2_str,"%d",&val2);printf("%#x\n",val2);
+    }else sscanf(val2_str,"%d",&val2);
     int reslut;
     char *r=malloc(32*sizeof(char));
 
