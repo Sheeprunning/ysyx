2c63816b8819da0fa86a46efef078c1a064ff231
tracer-ysyx <tracer@ysyx.org>
2025-07-25 14:38:48 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:38:48 up 49 min,  1 user,  load average: 0.78, 0.60, 0.52

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -47,7 +47,7 @@ static struct rule {
   {"\\(", TK_L_PRS},
   {"\\)", TK_R_PRS},
   {"0[xX][0-9a-fA-F]+", TK_0X},
-  {"\\$", TK_REG},
+  {"\\$[0-9a-zA-Z]", TK_REG},
   {"\\*", TK_MUL},
   {"/", TK_DIV},
   {"\\+", TK_PLUS},         // plus
