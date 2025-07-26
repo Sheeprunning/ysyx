d0c9c4c3d815eb751d268ceda88da1c7b093e0b8
tracer-ysyx <tracer@ysyx.org>
2025-07-26 21:55:03 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:55:03 up  5:00,  1 user,  load average: 0.76, 0.48, 2.03

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -57,6 +57,7 @@ void new_wp(char *args){
     wp -> next = head;
     head = wp;
     wp->wp_exp = strdup(args);
+    printf("args:%s exp:%s\n",args,wp->wp_exp);
     wp->value=data;
   }
   //sort();
