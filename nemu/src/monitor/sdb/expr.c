bc5de1d30960276c049eefdec12c9dc4090f2328
tracer-ysyx <tracer@ysyx.org>
2025-07-26 22:09:57 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:09:57 up  5:14,  1 user,  load average: 0.45, 0.32, 0.99

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -174,7 +174,7 @@ static bool make_token(char *e) {
             }break;
 
           case TK_SUB: 
-            if(nr_token==0){
+            if(nr_token==0&&certain_type(tokens[nr_token].type)!=2){
               tokens[nr_token].type=TK_SIGN_N;//认定该减号为负号
             }else if(certain_type(tokens[nr_token-1].type)!=1){//前一个不是数字
               if(certain_type(tokens[nr_token].type)==2){//之前已经认定为是符号
@@ -189,7 +189,7 @@ static bool make_token(char *e) {
             }break;
             
           case TK_PLUS:
-            if(nr_token==0){
+            if(nr_token==0&&certain_type(tokens[nr_token].type)!=2){
               tokens[nr_token].type=TK_SIGN_P;//认定该加号为正号
             }else if(certain_type(tokens[nr_token-1].type)!=1){//前一个不是数字
               if(certain_type(tokens[nr_token].type)==2){//之前已经认定为是符号
@@ -370,6 +370,8 @@ char* eval(int p, int q) {
       case TK_DER: reslut = paddr_read(val2, 4); break;
       default: assert(0);  break;
     }
+    free(val1_str);
+    free(val2_str);
     sprintf(r,"%d",reslut);
     return r;
   }
