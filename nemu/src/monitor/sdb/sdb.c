b3e91353f13bc51eebb28d9b4bfc5f953689e728
tracer-ysyx <tracer@ysyx.org>
2025-08-10 14:16:27 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:16:27 up  1:44,  1 user,  load average: 0.29, 0.21, 0.25

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -221,7 +221,7 @@ void init_sdb() {
   init_regex();
   /* Append for 环形缓冲 & 访存记录*/
   init_iringbuf();
-  init_mtrace("../../build/mtrace_log.txt");
+  init_mtrace("../../../../../build/mtrace_log.txt");
   /* Initialize the watchpoint pool. */
   init_wp_pool();
 }
