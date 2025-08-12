f21dc88c65a4453b8bd04203b63197a2bdf72dcc
tracer-ysyx <tracer@ysyx.org>
2025-08-12 21:44:58 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:44:58 up  6:08,  1 user,  load average: 0.41, 0.38, 0.29

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -7,16 +7,18 @@ void jal_ftrace(int rd,uint32_t pc,uint32_t target){
     int index;
     index=func_judge(target);
     if(index!=-1&&rd==1){//在riscv中，函数调用会把返回地址保存在目标寄存器 ra（x1）
-        printf("0x%x: call %s@0x%x\n", pc, func[index].name, target);
+        printf("0x%x: call [%s@0x%x]\n", pc, func[index].name, target);
     }
 }
 
 void jalr_ftrace(int32_t inst,int rd,int imm,uint32_t pc,uint32_t target){
+    int index;
     if (inst==0x00008067) {
-            printf("0x%x: ret\n", pc);
+            index=func_judge(pc);
+            printf("0x%x: ret [%s]\n", pc,func[index].name);
             return ;
         }
-    int index=func_judge(target);
+    index=func_judge(target);
     if(index!=-1)printf("0x%x: call %s@0x%x\n", pc, func[index].name, target);
 }
 
