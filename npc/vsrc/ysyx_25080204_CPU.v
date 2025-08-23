6ea8ca3ec268995589afcee8ef45447e70be3f07
tracer-ysyx <tracer@ysyx.org>
2025-08-24 00:25:16 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:25:16 up  8:42,  1 user,  load average: 1.84, 1.15, 0.75

--- npc/vsrc/ysyx_25080204_CPU.v
+++ npc/vsrc/ysyx_25080204_CPU.v
@@ -149,10 +149,10 @@ import "DPI-C" function void pmem_write_v(
 
 reg [31:0] rdata,wdata;
 reg [31:0] raddr,waddr;
-reg [31:0] len,w_len;
-reg write_ready;
-// assign wdata = src2;
-// assign waddr = result;
+reg [31:0] len;
+// reg write_ready;
+assign wdata = src2;
+assign waddr = result;
 assign raddr = result;
 
 always @(*)begin
@@ -171,19 +171,15 @@ always @(*) begin
     else begin
         rdata = 0;
     end
-    if (write_ready) begin // 有写请求时
-        pmem_write_v(waddr, w_len, wdata);
+    if (DM_w_en) begin // 有写请求时
+        pmem_write_v(waddr, len, wdata);
     end 
 end
 
-always @(posedge clk or posedge rst)begin
-    if(rst)write_ready<=0;
-    else if (DM_w_en) begin // 有写请求时
-            write_ready<=1;
-            waddr<=result;
-            wdata<=src2;
-            w_len<=len;
-        end 
-    else if (!DM_w_en)write_ready<=0;
-end
+// always @(posedge clk or posedge)begin
+//     if(rst)write_ready<=0;
+//     else if (DM_w_en) begin // 有写请求时
+//             write_ready<=1;
+//         end 
+// end
 endmodule
