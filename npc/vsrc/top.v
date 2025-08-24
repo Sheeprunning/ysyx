3568e1e57241436003b65563998c0244c178ea2f
tracer-ysyx <tracer@ysyx.org>
2025-08-24 11:31:47 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:31:47 up 31 min,  1 user,  load average: 0.96, 0.73, 0.51

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -23,8 +23,10 @@ ysyx_25080204_CPU CPU(
 );
 import "DPI-C" function void npc_ebreak_finish();
 always @(*)begin
-    if(inst==32'h100073)npc_ebreak_finish();
-    $display("[CLK %0t]ebreak",$time);
+    if(inst==32'h100073)begin
+        npc_ebreak_finish();
+        $display("[CLK %0t]ebreak",$time);
+    end
 end
 
 
