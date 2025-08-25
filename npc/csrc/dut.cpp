8014b332a370fbeca89dbbfa615f731af3ddf141
tracer-ysyx <tracer@ysyx.org>
2025-08-25 11:36:05 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:36:05 up 46 min,  1 user,  load average: 0.63, 0.27, 0.22

--- npc/csrc/dut.cpp
+++ npc/csrc/dut.cpp
@@ -118,5 +118,5 @@ void difftest_step(u_int32_t pc, u_int32_t npc) {
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
-  checkregs(&ref_r, npc);
+  checkregs(&ref_r, pc);
 }
--- npc/vsrc/ysyx_25080204_next_pc.v
+++ npc/vsrc/ysyx_25080204_next_pc.v
@@ -1,17 +1,16 @@
 module ysyx_25080204_next_pc (
-    input clk,
     input rst,
     input [31:0]pc,
     input bj_en,
     input [31:0]bj_addr,
     output reg [31:0]next_pc
 );
-    always @(posedge clk or posedge rst) begin
+    always @(*) begin
         if (rst)begin
-            next_pc<=32'h80000000;           
+            next_pc=32'h80000000;           
         end
         else begin
-            next_pc<=bj_en?bj_addr:pc+4;
+            next_pc=bj_en?bj_addr:pc+4;
         end
     end
 endmodule
