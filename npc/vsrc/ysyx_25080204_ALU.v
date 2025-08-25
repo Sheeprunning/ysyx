39744c7a668cf6b628d1ee68fcaf5eefae0b1eb6
tracer-ysyx <tracer@ysyx.org>
2025-08-25 15:55:42 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:55:42 up  5:04,  1 user,  load average: 1.00, 0.50, 0.28

--- npc/vsrc/ysyx_25080204_ALU.v
+++ npc/vsrc/ysyx_25080204_ALU.v
@@ -6,7 +6,24 @@ module ysyx_25080204_ALU(
     output reg Overflow,
     output reg CF
 );
-wire [31:0]B_n,add_result,sub_result,and_result,or_result,xor_result;
+localparam ALU_ADD = 4'b0000, ALU_SUB = 4'b0001, 
+        ALU_AND = 4'b0010, ALU_OR = 4'b0011,
+        ALU_XOR = 4'b0100, ALU_SLL = 4'b0101,
+        ALU_SRL = 4'b0110, ALU_SRA = 4'b1000,
+        ALU_SLT = 4'b1001, ALU_SLTU = 4'b1010;
+
+
+wire [31:0] B_n;
+wire [31:0] add_result;
+wire [31:0] sub_result;
+wire [31:0] and_result;
+wire [31:0] or_result;
+wire [31:0] xor_result;
+wire [31:0] sll_result;
+wire [31:0] srl_result;
+wire [31:0] sra_result;
+wire [31:0]slt_result;
+wire [31:0]sltu_result;
 
 assign B_n=B^{32{opcode[0]}}+{31'b0,opcode[0]};//({n{Cin}} ^ B) + Cin;一般补码是这样的
 assign {CF,add_result}={1'b0,A}+{1'b0,B_n};
@@ -15,13 +32,23 @@ assign Overflow=(A[31]==B_n[31])&&(A[31]!=add_result[31]);
 assign and_result=A&B;
 assign or_result=A|B;
 assign xor_result=A^B;
+assign sll_result=A<<B;
+assign srl_result=A>>B;
+assign sra_result=$signed(A)>>>$signed(B);
+assign slt_result={31'b0,$signed(A)<$signed(B)};
+assign sltu_result={31'b0,A<B};
 always@(*)begin
     case(opcode)
-        4'b0000:result=add_result;
-        4'b0001:result=sub_result;
-        4'b0010:result=and_result;
-        4'b0011:result=or_result;
-        4'b0100:result=xor_result;
+        ALU_ADD:result=add_result;
+        ALU_SUB:result=sub_result;
+        ALU_AND:result=and_result;
+        ALU_OR:result=or_result;
+        ALU_XOR:result=xor_result;
+        ALU_SLL:result=sll_result;
+        ALU_SRL:result=srl_result;
+        ALU_SRA:result=sra_result;
+        ALU_SLT:result=slt_result;
+        ALU_SLTU:result=sltu_result;
         default:result=0;
     endcase
 end
