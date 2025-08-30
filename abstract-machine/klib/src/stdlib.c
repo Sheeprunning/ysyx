7a7f33cb49ac4bc67ca26d5e9e6ff4a88a2ffe84
tracer-ysyx <tracer@ysyx.org>
2025-08-30 16:40:13 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:40:13 up  1:27,  1 user,  load average: 0.28, 0.60, 0.73

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -33,6 +33,8 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
   if (hbrk==0){
     hbrk = (void *)ROUNDUP(heap.start, 8);
   }
@@ -43,9 +45,7 @@ void *malloc(size_t size) {
   for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
     *p = 0;
   }
-// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-//   panic("Not implemented");
-// #endif
+#endif
   return old;
 }
 
