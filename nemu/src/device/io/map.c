3c02ead68f47362bfa5b71fae18070a1efccf644
tracer-ysyx <tracer@ysyx.org>
2025-08-30 21:56:50 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:56:50 up  4:48,  1 user,  load average: 0.64, 0.67, 0.89

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -59,7 +59,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  #ifdef CONFIG_DEVICE
   add_dtrace('R',addr,map->name,ret);
+  #endif
   return ret;
 }
 
@@ -69,5 +71,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+  #ifdef CONFIG_DEVICE
   add_dtrace('W',addr,map->name,data);
+  #endif
 }
