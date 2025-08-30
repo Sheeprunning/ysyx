5dbbc1c5a5812e5431917c1549a74700ac7abbc1
tracer-ysyx <tracer@ysyx.org>
2025-08-31 00:42:39 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:42:39 up  7:28,  1 user,  load average: 0.39, 0.30, 0.37

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -2,9 +2,9 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
+#define KEY_CODE_MASK 0x7FFF
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  
-  kbd->keycode = inl(KBD_ADDR)|KEYDOWN_MASK;
-  kbd->keydown =(kbd->keycode==AM_KEY_NONE)?0:1;
+  kbd->keydown = (inl(KBD_ADDR)&KEYDOWN_MASK)!=0;
+  kbd->keycode = inl(KBD_ADDR)&KEY_CODE_MASK;
 }
