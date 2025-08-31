e715be3d801841f87f5615448d4c2395caf81a70
tracer-ysyx <tracer@ysyx.org>
2025-08-31 14:42:06 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:42:06 up 12 min,  1 user,  load average: 1.43, 0.93, 0.50

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -9,5 +9,5 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t key_data = inl(KBD_ADDR);
   kbd->keydown = (key_data&KEYDOWN_MASK)!=0;
   kbd->keycode = key_data;
-  printf("down: %d code : %d",kbd->keydown,kbd->keycode);
+  printf("down: %d code : %d\n",kbd->keydown,kbd->keycode);
 }
