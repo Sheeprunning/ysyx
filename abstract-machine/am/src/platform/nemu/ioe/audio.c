b71294ce6f93fd9968f6c18b7b537f79a9eebaca
tracer-ysyx <tracer@ysyx.org>
2025-09-03 18:07:28 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:07:28 up  3:31,  1 user,  load average: 1.29, 1.18, 0.99

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -8,13 +8,13 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
-static uint8_t play_addr = 0;
+static uint32_t sbuf_rpos = 0;
 
 void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
   cfg->bufsize=inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
@@ -22,6 +22,7 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
   outl(AUDIO_CHANNELS_ADDR,ctrl->channels);
   outl(AUDIO_FREQ_ADDR,ctrl->freq);
   outl(AUDIO_SAMPLES_ADDR,ctrl->samples);
+  outl(AUDIO_INIT_ADDR,1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
@@ -37,8 +38,9 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   while(buf_size - count < len){
     count=inl(AUDIO_COUNT_ADDR);
   }
-  for(uint8_t *p= start;p<end;p++){
-    outb(AUDIO_SBUF_ADDR+p-start+play_addr,*p);
-    play_addr=(play_addr+1)%buf_size;
+  for(uint32_t i= 0;i<len;i++){
+    outb(AUDIO_SBUF_ADDR+sbuf_rpos,start[i]);
+    sbuf_rpos=(sbuf_rpos+1)%buf_size;
   }
+  outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR) + len);
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -30,21 +30,66 @@ enum {
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
-static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+static uint32_t sbuf_rpos = 0;
+
+static void audio_callback(void* userdata, Uint8* stream, int len){
+  uint32_t count =audio_base[reg_count];
+  
+  if (count == 0) {
+    memset(stream, 0, len);
+    return;
+  }
+
+  uint32_t bytes_to_read = (len < count) ? len : count;
+  int bytes_remain=sbuf_rpos+bytes_to_read-CONFIG_SB_SIZE;
+  if(bytes_remain>0){
+    memcpy(stream, sbuf + sbuf_rpos, bytes_to_read-bytes_remain);
+    memcpy(stream + bytes_to_read-bytes_remain, sbuf, bytes_remain);
+  }else{
+    memcpy(stream, sbuf + sbuf_rpos, bytes_to_read);
+  }
+  
+  sbuf_rpos = (sbuf_rpos + bytes_to_read) % CONFIG_SB_SIZE ;
+  audio_base[reg_count] -= bytes_to_read;
+
+  if (bytes_to_read < len) {
+    memset(stream + bytes_to_read, 0, len - bytes_to_read);
+  }
 }
+
+static void init_SDL_Audio(){
+  SDL_AudioSpec s = {};
+  s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
+  s.userdata = NULL;        // 不使用
+  s.freq = audio_base[reg_freq];
+  s.channels = audio_base[reg_channels];
+  s.samples = audio_base[reg_samples];
+  s.callback = audio_callback;
+  SDL_InitSubSystem(SDL_INIT_AUDIO);
+  SDL_OpenAudio(&s, NULL);
+  SDL_PauseAudio(0);
+}
+
+static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  if (!is_write)return ;
+  int index = offset / 4;
+  if (is_write){
+    switch (index){
+      case reg_init:
+        audio_base[reg_count]=0;
+        sbuf_rpos = 0;
+        init_SDL_Audio();
+        break;
+    }    
+  }
+
+}
+
 
 void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
-  SDL_AudioSpec s = {};
-  s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
-  s.userdata = NULL;        // 不使用
-  s.freq = audio_base[reg_samples];
-  s.channels = audio_base[reg_channels];
-  s.samples = audio_base[reg_samples];
-  SDL_InitSubSystem(SDL_INIT_AUDIO);
-  SDL_OpenAudio(&s, NULL);
-  SDL_PauseAudio(0);
+  audio_base[reg_sbuf_size]=CONFIG_SB_SIZE;
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
 #else
