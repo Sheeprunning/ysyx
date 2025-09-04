a639f2fbcba7c79fac2f2cd6081a78c075775c72
tracer-ysyx <tracer@ysyx.org>
2025-09-04 21:21:50 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:21:50 up  8:44,  1 user,  load average: 0.45, 0.43, 0.53

--- abstract-machine/scripts/riscv32e-npc.mk
+++ abstract-machine/scripts/riscv32e-npc.mk
@@ -3,6 +3,8 @@ include $(AM_HOME)/scripts/platform/npc.mk
 COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite
 LDFLAGS       += -melf32lriscv                    # overwrite
 
+CFLAGS  += -DISA_H=\"riscv/riscv.h\" # add by zzm 
+
 AM_SRCS += riscv/npc/libgcc/div.S \
            riscv/npc/libgcc/muldi3.S \
            riscv/npc/libgcc/multi3.c \
