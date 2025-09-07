a7867869d44d65f7e1549c1c0703278fed4fa30b
tracer-ysyx <tracer@ysyx.org>
2025-09-07 20:17:52 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:17:52 up  5:30,  1 user,  load average: 1.36, 0.64, 0.45

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,7 +5,7 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  printf("mcause:0x%8x mstatus:0x%8x mepc:0x%8x\n",c->mcause,c->mstatus,c->mepc);
+  printf("mcause:0x%08x mstatus:0x%08x mepc:0x%08x\n",c->mcause,c->mstatus,c->mepc);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -151,7 +151,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 110 ????? 0110011", rem    , R, R(rd) = (int32_t)src1 % (int32_t)src2);
   INSTPAT("0000001 ????? ????? 111 ????? 0110011", remu   , R, R(rd) = src1 % src2);
 
-  INSTPAT("0000000 00000 00000 000 00000 1110011", ecall  , N, s->dnpc=isa_raise_intr(0, s->pc));
+  INSTPAT("0000000 00000 00000 000 00000 1110011", ecall  , N, s->dnpc=isa_raise_intr(1, s->pc));
   INSTPAT("0000000 00001 00000 000 00000 1110011", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ???????", inv    , N, INV(s->pc));//输出无法匹配的pc
   INSTPAT_END();
