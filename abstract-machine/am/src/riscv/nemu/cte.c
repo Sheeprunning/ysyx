95355a80aa8c915f6712eb0d4908451809f317f2
tracer-ysyx <tracer@ysyx.org>
2025-09-15 22:55:11 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:55:11 up  2:54,  1 user,  load average: 1.01, 1.02, 0.66

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,8 +7,7 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  printf("0x%08x\n",0x80001520);
-  //printf("mcause:0x%08x mstatus:0x%08x mepc:0x%08x\n",c->mcause,c->mstatus,c->mepc);
+  printf("mcause:0x%08x mstatus:0x%08x mepc:0x%08x\n",c->mcause,c->mstatus,c->mepc);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
