bb55a1f2ffa00387f4609b570535abc1c34037f2
tracer-ysyx <tracer@ysyx.org>
2025-09-23 16:10:38 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:10:38 up 34 min,  1 user,  load average: 0.97, 0.82, 2.17

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -9,18 +9,41 @@ module top(
     output [31:0]pc
 );
 
-
+wire [31:0]rdata;
+wire DM_r_en,DM_w_en;
+wire [31:0]w_r_addr;
+wire [31:0]wdata;
+wire [1:0]mem_mask;
 
 ysyx_25080204_CPU CPU(
     .clk(clk),
     .rst(rst),
     .inst(inst),
+    .rdata(rdata),
     .Zero(Zero),
     .Overflow(Overflow),
     .CF(CF),
+    .DM_r_en(DM_r_en),
+    .DM_w_en(DM_w_en),
+    .w_r_addr(w_r_addr),
+    .wdata(wdata),
+    .mem_mask(mem_mask),
     .a0(a0),
     .pc(pc)
 );
+
+ysyx_25080204_DataMemory DRAM(
+    .clk(clk),
+    .rst(rst),
+    .DM_r_en(DM_r_en),
+    .DM_w_en(DM_w_en),
+    .raddr(w_r_addr),
+    .waddr(w_r_addr),
+    .wdata(wdata),
+    .mask(mem_mask),
+    .rdata(rdata)
+);
+
 import "DPI-C" function void npc_ebreak_finish();
 always @(*)begin
     if(inst==32'h100073)begin
--- npc/vsrc/ysyx_25080204_CPU.v
+++ npc/vsrc/ysyx_25080204_CPU.v
@@ -2,9 +2,15 @@ module ysyx_25080204_CPU(
     input clk,
     input rst,
     input [31:0]inst,
+    input [31:0]rdata,
     output Zero,
     output Overflow,
     output CF,
+    output DM_r_en,
+    output DM_w_en,
+    output [31:0]w_r_addr,
+    output [31:0]wdata,
+    output [1:0]mem_mask,
     output [31:0]a0,
     output [31:0]pc
 );
@@ -30,8 +36,7 @@ wire ALU_A_sel;
 wire ALU_B_sel;
 wire rf_w;
 wire [2:0]RF_data_sel;
-wire DM_r_en;
-wire DM_w_en;
+
 wire sext_en;
 wire CSR_wen;
 wire [1:0]mask;
@@ -43,7 +48,6 @@ wire [31:0]result;
 // wire Overflow;
 // wire CF;
 
-wire [31:0]rdata;
 
 wire [31:0]sext_out_data;
 
@@ -83,6 +87,8 @@ ysyx_25080204_Decoder Decoder(
     .func3(func3)    
 );
 
+assign wdata=src2;
+
 ysyx_25080204_RegisterFile RF (
     .clk(clk),
     .rst(rst),
@@ -114,6 +120,8 @@ ysyx_25080204_ControlUnit CU(
 assign A=ALU_A_sel?src1:pc;
 assign B=ALU_B_sel?src2:imm_num;
 assign a0=RF.rf[10];
+
+assign mem_mask=mask;
 
 wire beq_taken=(src1==src2);
 wire bne_taken=!beq_taken;
@@ -161,19 +169,7 @@ ysyx_25080204_ALU ALU(
     .CF(CF)
 );
 
-
-
-ysyx_25080204_DataMemory DM(
-    .clk(clk),
-    .rst(rst),
-    .DM_r_en(DM_r_en),
-    .DM_w_en(DM_w_en),
-    .raddr(result),
-    .waddr(result),
-    .wdata(src2),
-    .mask(mask),
-    .rdata(rdata)
-);
+assign w_r_addr=result;
 
 ysyx_25080204_sext SEXT(
     .sext_en(sext_en),
