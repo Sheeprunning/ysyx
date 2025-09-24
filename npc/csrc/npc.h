4576d93c07368e97316e0aa34f01fb787a837b9e
tracer-ysyx <tracer@ysyx.org>
2025-09-24 17:36:30 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:36:30 up  2:36,  1 user,  load average: 0.39, 0.31, 0.23

--- npc/csrc/npc.h
+++ npc/csrc/npc.h
@@ -5,7 +5,7 @@
 #include "dut.h"
 
 // #define CONFIG_FTRACE 1
-// #define CONFIG_ITRACE 1
+#define CONFIG_ITRACE 1
 // #define DIFFTEST 1
 
 
---
+++ npc/vsrc/ysyx_25080204_1_IFU.v
@@ -0,0 +1,6 @@
+module ysyx_25080204_1_IFU(
+    
+
+);
+
+endmodule
