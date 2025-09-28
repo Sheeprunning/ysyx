a22e2e3ec60f5730eb790d4930efd883a3e952d6
tracer-ysyx <tracer@ysyx.org>
2025-09-28 15:30:51 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:30:51 up  2:42,  1 user,  load average: 0.88, 0.40, 0.18

---
+++ npc/vsrc/ysyx_25080204_2_IDU.v
@@ -0,0 +1,95 @@
+module ysyx_25080204_2_IDU(
+    //decoder's signal
+    input [31:0]inst,
+    output [4:0]rd,
+    output [4:0]rs1,
+    output [4:0]rs2,
+    output [2:0]func3,
+    output [6:0]opcode,
+    output [31:0]imm_num,
+    //CU's signal
+    output [3:0]alu_op,
+    output ALU_A_sel,
+    output ALU_B_sel,
+    output rf_w,
+    output [2:0]RF_data_sel,
+    output sext_en,
+    output CSR_wen,
+    output [1:0]mask,
+    output DM_r_en,
+    output DM_w_en,
+    //judge the bj_en
+    input [31:0]src1,
+    input [31:0]src2,
+    output bj_en,
+    //CSR
+    output en_ecall,
+    output en_mret,
+    output csr_jen
+);
+
+wire [6:0]func7;
+
+ysyx_25080204_Decoder Decoder(
+    .inst(inst),
+    .rd(rd),
+    .rs1(rs1),
+    .rs2(rs2),
+    .opcode(opcode),
+    .imm_num(imm_num),
+    .func7(func7),
+    .func3(func3)    
+);
+
+
+ysyx_25080204_ControlUnit CU(
+    .opcode(opcode),
+    .func7(func7),
+    .func3(func3),
+    .alu_op(alu_op),
+    .ALU_A_sel(ALU_A_sel),
+    .ALU_B_sel(ALU_B_sel),
+    .rf_w(rf_w),
+    .RF_data_sel(RF_data_sel),
+    .DM_r_en(DM_r_en),
+    .DM_w_en(DM_w_en),
+    .sext_en(sext_en),
+    .CSR_wen(CSR_wen),
+    .mask(mask)
+);
+wire beq_taken=(src1==src2);
+wire bne_taken=!beq_taken;
+wire blt_taken=($signed(src1)<$signed(src2));
+wire bge_taken=!blt_taken;
+wire bltu_taken=(src1<src2);
+wire bgeu_taken=!bltu_taken;
+reg  bj_en_reg;
+//bj_en
+always @(*) begin
+    case(opcode)
+        7'b1100011:begin
+            case(func3)
+                3'b000: bj_en_reg = beq_taken;  // beq
+                3'b001: bj_en_reg = bne_taken;  // bne
+                3'b100: bj_en_reg = blt_taken;  // blt
+                3'b101: bj_en_reg = bge_taken;  // bge
+                3'b110: bj_en_reg = bltu_taken; // bltu
+                3'b111: bj_en_reg = bgeu_taken; // bgeu
+                default: bj_en_reg = 1'b0;      // 默认情况
+            endcase
+        end
+        7'b1100111,7'b1101111: //jalr & jal
+            bj_en_reg=1;
+        default:
+            bj_en_reg=0;
+                    
+    endcase
+    
+end
+assign bj_en=bj_en_reg;
+//CSR
+assign en_ecall=(inst==32'h00000073);
+assign en_mret=(inst==32'h30200073);
+assign csr_jen=en_ecall|en_mret;
+
+endmodule
--- npc/vsrc/ysyx_25080204_CPU.v
+++ npc/vsrc/ysyx_25080204_CPU.v
@@ -24,10 +24,10 @@ wire [1:0]current_privilege=2'b11;
 wire [4:0]rd;
 wire [4:0]rs1;
 wire [4:0]rs2;
+wire [2:0]func3;
 wire [6:0]opcode;
 wire [31:0]imm_num;
-wire [6:0]func7;
-wire [2:0]func3;
+
 
 wire [31:0]RF_w_data;
 wire [31:0]src1;
@@ -38,7 +38,7 @@ wire ALU_A_sel;
 wire ALU_B_sel;
 wire rf_w;
 wire [2:0]RF_data_sel;
-
+wire bj_en;
 wire sext_en;
 wire CSR_wen;
 wire [1:0]mask;
@@ -75,15 +75,33 @@ ysyx_25080204_1_IFU IFU(
 
 assign next_pc_for_inst=next_pc;
 
-ysyx_25080204_Decoder Decoder(
+ysyx_25080204_2_IDU IDU(
     .inst(inst),
     .rd(rd),
     .rs1(rs1),
     .rs2(rs2),
+    .func3(func3),
     .opcode(opcode),
-    .imm_num(imm_num),
-    .func7(func7),
-    .func3(func3)    
+    .imm_num(imm_num), 
+
+    .alu_op(alu_op),
+    .ALU_A_sel(ALU_A_sel),
+    .ALU_B_sel(ALU_B_sel),
+    .rf_w(rf_w),
+    .RF_data_sel(RF_data_sel),
+    .DM_r_en(DM_r_en),
+    .DM_w_en(DM_w_en),
+    .sext_en(sext_en),
+    .CSR_wen(CSR_wen),
+    .mask(mask),
+
+    .src1(src1),
+    .src2(src2),
+    .bj_en(bj_en),
+
+    .en_ecall(en_ecall),
+    .en_mret(en_mret),
+    .csr_jen(csr_jen)  
 );
 
 assign wdata=src2;
@@ -101,21 +119,6 @@ ysyx_25080204_RegisterFile RF (
     .src2(src2)
 );
 
-ysyx_25080204_ControlUnit CU(
-    .opcode(opcode),
-    .func7(func7),
-    .func3(func3),
-    .alu_op(alu_op),
-    .ALU_A_sel(ALU_A_sel),
-    .ALU_B_sel(ALU_B_sel),
-    .rf_w(rf_w),
-    .RF_data_sel(RF_data_sel),
-    .DM_r_en(DM_r_en),
-    .DM_w_en(DM_w_en),
-    .sext_en(sext_en),
-    .CSR_wen(CSR_wen),
-    .mask(mask)
-);
 
 assign A=ALU_A_sel?src1:pc;
 assign B=ALU_B_sel?src2:imm_num;
@@ -123,41 +126,6 @@ assign a0=RF.rf[10];
 
 assign mem_mask=mask;
 
-wire beq_taken=(src1==src2);
-wire bne_taken=!beq_taken;
-wire blt_taken=($signed(src1)<$signed(src2));
-wire bge_taken=!blt_taken;
-wire bltu_taken=(src1<src2);
-wire bgeu_taken=!bltu_taken;
-reg bj_en;
-//bj_en
-always @(*) begin
-    if(rst)bj_en=0;
-    else begin
-        case(opcode)
-            7'b1100011:begin
-                case(func3)
-                    3'b000: bj_en = beq_taken;  // beq
-                    3'b001: bj_en = bne_taken;  // bne
-                    3'b100: bj_en = blt_taken;  // blt
-                    3'b101: bj_en = bge_taken;  // bge
-                    3'b110: bj_en = bltu_taken; // bltu
-                    3'b111: bj_en = bgeu_taken; // bgeu
-                    default: bj_en = 1'b0;      // 默认情况
-                endcase
-            end
-            7'b1100111,7'b1101111: //jalr & jal
-                bj_en=1;
-            default:
-                bj_en=0;
-                        
-        endcase
-    end
-end
-
-assign en_ecall=(inst==32'h00000073);
-assign en_mret=(inst==32'h30200073);
-assign csr_jen=en_ecall|en_mret;
 
 ysyx_25080204_ALU ALU(
     .opcode(alu_op),
