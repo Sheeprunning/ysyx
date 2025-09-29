1f889c14afb7d48ac7b80ba2231af919e57a45b0
tracer-ysyx <tracer@ysyx.org>
2025-09-29 19:24:06 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:24:06 up  7:22,  1 user,  load average: 1.46, 0.76, 0.44

--- npc/csrc/watchpoint.cpp
+++ npc/csrc/watchpoint.cpp
@@ -81,7 +81,7 @@ void free_wp(int NO){
 WP* compare_watchpoint(bool *success,int *change){
   WP * wp = head;
   if(!wp)return NULL;
-  printf("Comparing the watchpoint...!\n");
+//   printf("Comparing the watchpoint...!\n");
   while(wp != NULL){
     *change=expr(wp->wp_exp,success);
     if(!success){//计算表达式错误
