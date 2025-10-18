411118550477942a8ac5e810aee92e52f99c894c
tracer-ysyx <tracer@ysyx.org>
2025-10-18 20:32:06 +0800

    > compile NEMU ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:32:06 up  7:20,  1 user,  load average: 0.36, 0.49, 0.67

--- npc/vsrc/ysyx_25080204.v
+++ npc/vsrc/ysyx_25080204.v
@@ -435,7 +435,7 @@ always @(posedge clk or posedge rst) begin
         inst_araddr_reg <= 32'b0;
         inst_rready_reg <= 1'b0;
         inst_reg <= 32'h00000013; 
-        inst_stall <= 1'b0;
+        inst_stall <= 1'b1;
         inst_state <= INST_RESET;
     end else begin
         case(inst_state)
