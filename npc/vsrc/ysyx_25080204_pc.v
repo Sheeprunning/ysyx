408a9ac64cd8188208f0a57fb744fd3f48174a99
tracer-ysyx <tracer@ysyx.org>
2025-10-20 10:54:06 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:54:06 up 33 min,  1 user,  load average: 1.03, 0.84, 0.78

--- npc/vsrc/ysyx_25080204_pc.v
+++ npc/vsrc/ysyx_25080204_pc.v
@@ -6,20 +6,31 @@ module ysyx_25080204_pc (
     output reg [31:0]pc
 );
     
-    reg pc_stall;
+    // reg pc_stall;
+    // always @(posedge clk or posedge rst) begin
+    //     if (rst)begin
+    //         pc<=32'h20000000; 
+    //         pc_stall<=1'b1;     //每个周期取指肯定有延迟，pc变成next默认先保持一周期 
+    //     end
+    //     else if(!stall)begin
+    //         if(pc_stall)pc_stall<=1'b0;//第一个周期暂停pc，等待
+    //         else begin
+    //         $display("[CLK %0t]EXCUTE:pc:0x%08x next_pc:0x%08x", $time,pc,next_pc);
+    //             pc<=next_pc;   
+    //             //pc_stall<=1'b1; 
+    //         end
+            
+    //     end            
+    // end
     always @(posedge clk or posedge rst) begin
         if (rst)begin
             pc<=32'h20000000; 
-            pc_stall<=1'b1;     //每个周期取指肯定有延迟，pc变成next默认先保持一周期 
         end
         else if(!stall)begin
-            if(pc_stall)pc_stall<=1'b0;//第一个周期暂停pc，等待
-            else begin
             $display("[CLK %0t]EXCUTE:pc:0x%08x next_pc:0x%08x", $time,pc,next_pc);
                 pc<=next_pc;   
                 //pc_stall<=1'b1; 
             end
-            
-        end            
+                       
     end
 endmodule
