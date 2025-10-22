1c0e9c586a1b8e9fad19dc8afb89dc6da86b25b7
tracer-ysyx <tracer@ysyx.org>
2025-10-22 17:24:35 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:24:35 up  1:22,  1 user,  load average: 0.60, 0.30, 0.24

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -32,10 +32,10 @@ void rom_to_sram(){
 
 void uart_init() {
   uint8_t lcr = inb(UART_LCR);
-  outb(UART_LCR,lcr | 0x80);
+  outb(UART_LCR,0x80);
   outb(UART_MSB,0);
   outb(UART_LSB,101);
-  outb(UART_LCR,0x03);
+  outb(UART_LCR,lcr & 0x7F);
 }
 
 void _trm_init() {
