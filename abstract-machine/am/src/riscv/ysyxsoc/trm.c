c5c570747d54212c229167be5747e904a7f94f96
tracer-ysyx <tracer@ysyx.org>
2025-10-23 15:41:00 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:41:00 up 51 min,  1 user,  load average: 0.17, 0.40, 0.34

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -15,8 +15,8 @@ Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 void putch(char ch) {
-  while( (inb(UART_LSR) & 0x40) == 0);
-  outb(UART_THR, ch);
+  
+  outb(UART_THR, ch);while( (inb(UART_LSR) & 0x40) == 0);
 }
 
 void halt(int code) {
