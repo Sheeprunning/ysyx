c952bbe160d6df2be1fe55726ed58ef1382dec83
tracer-ysyx <tracer@ysyx.org>
2025-10-25 16:47:11 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:47:11 up  1:41,  1 user,  load average: 0.96, 0.90, 0.54

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -126,7 +126,7 @@ extern "C"
     }
 }
 
-extern "C" void flash_read(int32_t addr, int32_t *data) { cout<<addr-0x30000000<<endl; }
+extern "C" void flash_read(int32_t addr, int32_t *data) { cout<<addr-0x30000000<<endl;*data=addr-0x30000000; }
 extern "C" void mrom_read(int32_t addr, int32_t *data) { 
   //printf("read mrom[0x%08x]=0x%08x\n",addr,pmem_read(addr,4));
   *data=pmem_read(addr&0xFFFFFFFC,4); 
