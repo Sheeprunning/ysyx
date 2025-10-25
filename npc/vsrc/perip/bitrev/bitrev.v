8116c4a1bcf2d8e5a3b29c4a42144af1b16e4b7d
tracer-ysyx <tracer@ysyx.org>
2025-10-25 21:18:41 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:18:41 up  5:51,  1 user,  load average: 0.98, 0.48, 0.35

--- npc/vsrc/perip/bitrev/bitrev.v
+++ npc/vsrc/perip/bitrev/bitrev.v
@@ -4,5 +4,46 @@ module bitrev (
   input  mosi,
   output miso
 );
-  assign miso = 1'b1;
+  parameter [1:0] data_i_t = 0, data_o_t = 1;
+  reg [1:0]state;
+  reg [7:0]data_i;
+  reg [7:0]data_o;
+  reg [2:0]counter;
+  wire rst=ss;
+  assign miso =ss? 1'b1:(state==data_o_t)?data_o[7]:1'b1;
+
+  always@(posedge sck or posedge rst)begin
+    if(rst) counter<=0;
+    else begin
+      case(state)
+        data_i_t: counter<=(counter==3'd7)?0:counter+1;
+        data_o_t: counter<=0;
+        default: $display("ERROR!");
+      endcase
+    end
+  end
+
+  always@(posedge sck or posedge rst)begin
+    if(rst) state<=data_i_t;
+    else begin
+      case(state)
+        data_i_t: state<=(counter==3'd7)?data_o_t:state;
+        data_o_t: state<=(counter==3'd7)?data_i_t:state;
+        default: $display("ERROR!");
+      endcase
+    end
+  end
+
+  always@(negedge sck or posedge rst)begin
+    if(rst)data_i<=8'b0;
+    else if(state==data_i_t) data_i<={data_i[6:0],mosi};
+  end
+
+  always@(posedge sck)begin
+    if(counter==3'd7)data_o<=data_i;
+    else if(state==data_o_t) begin
+      data_o<={data_o[6:0],1'b0};
+    end
+  end
+
 endmodule
