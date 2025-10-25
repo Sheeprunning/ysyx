da6721a3cb28874bc1015b3a3058c4a09c7a306f
tracer-ysyx <tracer@ysyx.org>
2025-10-25 17:22:20 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:22:20 up  2:14,  1 user,  load average: 1.05, 0.87, 0.74

--- npc/vsrc/perip/flash/flash.v
+++ npc/vsrc/perip/flash/flash.v
@@ -65,9 +65,9 @@ module flash (
 
   always@(posedge sck or posedge reset) begin
     if (reset) addr <= 24'd0;
-    else if (state == addr_t && counter < 8'd23)
+    else if (state == addr_t && counter < 8'd23)begin
       addr <= { addr[22:0], mosi };
-      $display("\033[1;33m addr:0x%08x\033[0m",{ addr[22:0], mosi });
+      $display("\033[1;33m addr:0x%08x\033[0m",{ addr[22:0], mosi });end
   end
 
   wire [31:0] data_bswap = {rdata[7:0], rdata[15:8], rdata[23:16], rdata[31:24]};
