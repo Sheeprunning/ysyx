e8152c91d0e0abb42670bcf5b6421d297d00e916
tracer-ysyx <tracer@ysyx.org>
2025-11-02 14:46:14 +0800

    > sim RTL ysyx_22040001 赵仲明 Linux DESKTOP-5MGH8BM 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:46:14 up 35 min,  1 user,  load average: 0.72, 0.40, 0.51

--- npc/vsrc/perip/bitrev/bitrev.v
+++ npc/vsrc/perip/bitrev/bitrev.v
@@ -4,61 +4,47 @@ module bitrev (
   input  mosi,
   output miso
 );
+  parameter [1:0] data_i_t = 0, data_o_t = 1;
+  reg [1:0]state;
+  reg [7:0]data_i;
+  reg [7:0]data_o;
+  reg [3:0]counter;
+  wire rst=ss;
+  assign miso =ss? 1'b1:(state==data_o_t)?data_o[7]:1'b1;
 
-  reg [7:0] input_shift;
-  reg [7:0] output_shift;
-  reg [7:0] reversed_data;
-  reg [3:0] bit_cnt;
-  reg data_ready;
-  
-  wire rst = ss;
-  
-  // miso输出：直接输出output_shift的最高位
-  assign miso = data_ready?output_shift[7]:1'b1;
-
-  always @(negedge sck or posedge rst) begin
-    if (rst) begin
-      input_shift <= 8'b0;
-      output_shift <= 8'b0;
-      reversed_data <= 8'b0;
-      bit_cnt <= 4'b0;
-      data_ready <= 1'b0;
-    end else begin
-      // 输入采样
-      input_shift <= {input_shift[6:0], mosi};
-      bit_cnt <= bit_cnt + 4'b1;
-      
-      if (bit_cnt == 4'd7) begin
-        // 接收完8位，立即进行位反转
-        reversed_data <= {input_shift[0], input_shift[1], input_shift[2], input_shift[3],
-                         input_shift[4], input_shift[5], input_shift[6], mosi};
-        data_ready <= 1'b1;
-      end else if (bit_cnt == 4'd15) begin
-        // 16位传输结束，重置
-        bit_cnt <= 4'b0;
-        data_ready <= 1'b0;
-      end
+  always@(negedge sck or posedge rst)begin
+    if(rst) counter<=0;
+    else begin
+      case(state)
+        data_i_t: counter<=counter+1;
+        data_o_t: counter<=(counter==4'd15)?0:counter+1;
+        default: $display("ERROR!");
+      endcase
     end
   end
 
-  always @(negedge sck or posedge rst) begin
-    if (rst) begin
-      output_shift <= 8'b0;
-    end else begin
-    if (bit_cnt == 4'd7) begin
-          // 第一次输出时加载完整反转数据
-          output_shift <= {input_shift[0], input_shift[1], input_shift[2], input_shift[3],
-                         input_shift[4], input_shift[5], input_shift[6], mosi};
-          $display("%08b",{input_shift[0], input_shift[1], input_shift[2], input_shift[3],
-                         input_shift[4], input_shift[5], input_shift[6], mosi});
-        end
-      else if (data_ready) begin
-          // 后续输出移位
-          output_shift <= {output_shift[6:0], 1'b0};
-      end else begin
-        // 无数据时输出0或其他默认值
-        output_shift <= 8'b1;
-      end
+  always@(negedge sck or posedge rst)begin
+    if(rst) state<=data_i_t;
+    else begin
+      case(state)
+        data_i_t: state<=(counter==3'd7)?data_o_t:state;
+        data_o_t: state<=(counter==3'd15)?data_i_t:state;
+        default: $display("ERROR!");
+      endcase
+    end
+  end
+
+  always@(negedge sck or posedge rst)begin
+    if(rst)data_i<=8'b0;
+    else if(state==data_i_t) data_i<={mosi,data_i[7:1]};
+  end
+
+  always@(negedge sck)begin
+    if(counter==3'd7)begin
+      data_o<={mosi,data_i[7:1]};
+    end
+    else if(state==data_o_t) begin
+      data_o<={data_o[6:0],1'b0};
     end
   end
 
