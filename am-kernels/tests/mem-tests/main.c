1ea231fa9eb25ad1777a83a27fca91f2ea872e5f
25080204-ZhaoZhongming <2913996394@qq.com>
2025-12-03 20:50:56 +0800

    添加环境配置脚本

---
+++ am-kernels/tests/mem-tests/main.c
@@ -0,0 +1,5 @@
+#include "sdram_test.h"
+int main() {
+    sdram_test();
+    return 0;
+}
---
+++ environment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CURRENT_DIR=$(pwd)
+AM_HOME="$CURRENT_DIR/abstract-machine"
+export AM_HOME
+YSYXSOC_HOME="$CURRENT_DIR/ysyxSoC"
+export YSYXSOC_HOME
+NPC_HOME="$CURRENT_DIR/npc"
+export NPC_HOME
+NEMU_HOME="$CURRENT_DIR/nemu"
+export NEMU_HOME
